8b03ec757d9035e8f94aa02022bfa4a1d75f6abb
umme habiba <umme.a.qadri@gmail.com>
2020-12-28 01:38:23 +0530

    added css for input

--- index.js
+++ index.js
@@ -1,29 +1,90 @@
-createTable();
-function createTable() {
+createGridMatrix();
+createCheckersMatrix();
+createOddMatrix();
+function createGridMatrix() {
     var a, b, tableEle, rowEle, colEle;
     var myTableDiv = document.getElementById("dynamic_table");
-    var dynamicTableTwo = document.getElementById("dynamic_table_two");
-    var dynamicTableThree = document.getElementById("dynamic_table_three");
-    
-
-    a = 5;
-    b = 5;
-    if (a == "" || b == "") {
+    const row = 5;
+    const col= 5;
+    if (row == "" || col == "") {
         alert("Please enter some numeric value");
     } else {
         tableEle = document.createElement('table');
-        for (var i = 1; i <= a; i++) {
+        for (var i = 1; i <= row; i++) {
             rowEle = document.createElement('tr');
             rowEle.className = "table_rows";
             rowEle.id = "checkers_"+i;
-            for (var j = 1; j <= b; j++) {
+            for (var j = 1; j <= col; j++) {
                 let text = document.createTextNode("");
                 colEle = document.createElement('td');
                 colEle.className = "circle";
                 colEle.appendChild(text);
                 rowEle.appendChild(colEle);
                 colEle.id = "checkers_"+i+''+j;
+            }
+            console.log("table",rowEle);
+            tableEle.appendChild(rowEle);
+        }
+        myTableDiv.innerHTML = tableEle.outerHTML ;
+    }
+}
+
+
+function createCheckersMatrix() {
+    var a, b, tableEle, rowEle, colEle;
+    var dynamicTableTwo = document.getElementById("dynamic_table_two");
+   
+    const row = 5;
+    const col= 5;
+    if (row == "" || row == "") {
+        alert("Please enter some numeric value");
+    } else {
+        tableEle = document.createElement('table');
+        for (var i = 1; i <= row; i++) {
+            rowEle = document.createElement('tr');
+            rowEle.className = "table_rows";
+            rowEle.id = "checkers_"+i;
+            for (var j = 1; j <= col; j++) {
+                let text = document.createTextNode("");
+                colEle = document.createElement('td');
+                colEle.appendChild(text);
+                colEle.className = "circle";
+                rowEle.appendChild(colEle);
+                colEle.id = "checkers_"+i+''+j;
                 checkersCircle(rowEle,colEle);
+                //oddCircle(rowEle,colEle)
+                
+                
+            }
+            console.log("table",rowEle);
+            tableEle.appendChild(rowEle);
+        }
+        dynamicTableTwo.innerHTML = tableEle.innerHTML;
+    }
+}
+
+function createOddMatrix() {
+    var tableEle, rowEle, colEle;
+    var dynamicTableThree = document.getElementById("dynamic_table_three");
+    
+
+    const row = 5;
+    const col= 5;
+    if (row == "" || col == "") {
+        alert("Please enter some numeric value");
+    } else {
+        tableEle = document.createElement('table');
+        for (var i = 1; i <= row; i++) {
+            rowEle = document.createElement('tr');
+            rowEle.className = "table_rows";
+            rowEle.id = "checkers_"+i;
+            for (var j = 1; j <= col; j++) {
+                let text = document.createTextNode("");
+                colEle = document.createElement('td');
+                colEle.className = "circle";
+                colEle.appendChild(text);
+                rowEle.appendChild(colEle);
+                colEle.id = "checkers_"+i+''+j;
                 oddCircle(rowEle,colEle)
                 
                 
@@ -31,13 +92,9 @@ function createTable() {
             console.log("table",rowEle);
             tableEle.appendChild(rowEle);
         }
-        myTableDiv.innerHTML = tableEle.outerHTML ;
-        dynamicTableTwo.innerHTML = tableEle.innerHTML;
         dynamicTableThree.innerHTML = tableEle.innerHTML;
     }
 }
-
-
 
 function checkersCircle(rowEle,colEle){
     const rowIdVal = rowEle.id.split("_");
@@ -73,11 +130,6 @@ function isOdd(num) {
     return num % 2;
 }
 
-
-
-
-
-
 function formSubmit (form) {
     const boxWidth = form.box_width.value;
     const boxHeight = form.box_height.value;
@@ -103,17 +155,18 @@ function formSubmit (form) {
 
         };
 
-        createMatrix(formData)
+        createResultMatrix(formData)
     }
 }
 
 
-function createMatrix(data){
-    var a, b, tableEle, rowEle, colEle;
-    var tableTag = document.getElementById("dynamic_table_four");
+function createResultMatrix(data){
+    let  tableEle, rowEle, colEle;
+    let tableTag = document.getElementById("dynamic_table_four");
     
     tableTag.parentElement.style.width = data.boxWidth + "px";
     tableTag.parentElement.style.height = data.boxHeight + "px";
+    tableTag.parentElement.style.backgroundColor = data.boxColor;
     
     const rows = data.boxRows;
     const cols = data.boxCols;
@@ -129,12 +182,16 @@ function createMatrix(data){
                 let text = document.createTextNode("");
                 colEle = document.createElement('td');
                 colEle.className = "circle";
+                colEle.style.backgroundColor = data.dotsColor;
+                colEle.style.borderRadius = data.circleRadius + "%";
                 colEle.appendChild(text);
                 rowEle.appendChild(colEle);
-                colEle.id = "checkers_"+i+''+j;
-                //checkersCircle(rowEle,colEle);
-               // oddCircle(rowEle,colEle)
-                
+                colEle.id = "matcheckers_"+i+''+j;
+                if(data.selectedPattern === 'checkers'){
+                    checkersCircle(rowEle,colEle);
+                } else if(data.selectedPattern === 'oddcircle'){
+                    oddCircle(rowEle,colEle)
+                }
                 
             }
             tableEle.appendChild(rowEle);
